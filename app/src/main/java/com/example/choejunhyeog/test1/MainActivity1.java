b633fe064b443fa251b8c6c5db0c769bb6e342b9
Garamantes <Garamantes16@gmail.com>
2018-09-14 01:06:56 +0900

    test1

---
+++ app/src/main/java/com/example/choejunhyeog/test1/MainActivity1.java
@@ -0,0 +1,15 @@
+package com.example.choejunhyeog.test1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);    //여기서 가져다 씀!!!!
+    }
+}
+
+
