52bfa56826f194f43d99930e352ec0a2249971b9
Garamantes <Garamantes16@gmail.com>
2018-10-08 01:14:54 +0900

    add basic Toolbar

--- app/src/main/java/com/example/choejunhyeog/test1/BottomNavActivity.java
+++ app/src/main/java/com/example/choejunhyeog/test1/BottomNavActivity.java
@@ -3,6 +3,7 @@ package com.example.choejunhyeog.test1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.content.Context;
 import android.support.annotation.NonNull;
@@ -24,35 +25,21 @@ public class BottomNavActivity extends BaseActivity {
     private Fragment activeFragment;
     private final FragmentManager fm = getSupportFragmentManager();
 
+    Toolbar myToolbar;
+
+
     private BottomNavigationView bottomNavigationView;
 
     private TextView mTextMessage;
-
-    /*private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_nav);
+
+        //툴바 부분
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         setArguments();
         initFragments();
@@ -67,14 +54,6 @@ public class BottomNavActivity extends BaseActivity {
         textView.setText(s);
 
         bottomNavigationView.setSelectedItemId(R.id.nav_bottom_feed);
-
-
-
-
-        //기존 코드
-        //mTextMessage = (TextView) findViewById(R.id.message);
-        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
     }
 
--- app/src/main/java/com/example/choejunhyeog/test1/MainActivity1.java
+++ app/src/main/java/com/example/choejunhyeog/test1/MainActivity1.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.graphics.Color;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,14 +19,52 @@ import java.util.Random;
 public class MainActivity1 extends BaseActivity {
 
     int i=1;
+    Toolbar myToolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);    //여기서 가져다 씀!!!!
 
+        //툴바 부분
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         //외부 클래스에서 선언한 변수를 onClick()에서 사용하기 위해 final을 부텨 줌, 이게 싫으면 해당 클래스 안에 선언할 것
         //final TextView textView1 = (TextView) findViewById(R.id.text1);
-        Button button1 = (Button) findViewById(R.id.button1);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView imageView = (ImageView) findViewById(R.id.img1);
+                TextView textView1 = (TextView) findViewById(R.id.text1);
+
+                i = i * -1;
+                if(i < 1){
+                    imageView.setImageResource(R.drawable.android2);
+                }else{
+                    imageView.setImageResource(R.drawable.android3);
+                }
+
+                Date date = new Date();
+                SimpleDateFormat tf = new SimpleDateFormat("hh:mm:ss a");
+                String time = tf.format(date);
+
+                textView1.setText("지금 시각은 " + time + "입니다.");
+
+                Random rand = new Random();
+                int color = Color.rgb(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+                textView1.setTextColor(color);
+
+                view.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+            }
+        });
+
+        /*
+        Button button1 = (Button) findViewById(R.id.button2);
         button1.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,6 +100,7 @@ public class MainActivity1 extends BaseActivity {
 
             }
         });
+        */
 
         Button button3 = (Button) findViewById(R.id.button3);
         button3.setOnClickListener(new Button.OnClickListener() {
